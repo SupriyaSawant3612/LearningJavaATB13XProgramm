663746fd06998399b208a593b8cc8d788f938dd4
supri <supriyasawant3612@gmail.com>
2025-07-25 11:18:59 +0530

    Keywords identifiers

---
+++ src/ex_01_Java_Basic/Lab009_Keywords_Identifiers.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab009_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi, Supriya");
+//        Blue colour words present are key words.
+//        'main' is not consider a keyword
+//      'Lab009_Keywords_Identifiers' this is called identifier.
+
+    }
+}
